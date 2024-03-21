6ac41e3dfe6daad36755984e0d909e5c8b423ae8
Andrey Anisimov <qwertfy@ya.ru>
2024-03-21 11:14:49 +0300

    Update requirements.txt.

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,24 +1,59 @@
 """View-function for birthday calculation."""
 
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
 from .forms import BirthdayForm
 from .models import Birthday
+from .utils import calculate_birthday_countdown
 
 
-def birthday(request):
-    """View-function for birthday calculation."""
-
-    form = BirthdayForm(request.POST or None)
-    # Добавляем его в словарь контекста под ключом form:
+# Добавим опциональный параметр pk.
+def birthday(request, pk=None):
+    # Если в запросе указан pk (если получен запрос на редактирование объекта):
+    if pk is not None:
+        # Получаем объект модели или выбрасываем 404 ошибку.
+        instance = get_object_or_404(Birthday, pk=pk)
+    # Если в запросе не указан pk
+    # (если получен запрос к странице создания записи):
+    else:
+        # Связывать форму с объектом не нужно, установим значение None.
+        instance = None
+    # Передаём в форму либо данные из запроса, либо None. 
+    # В случае редактирования прикрепляем объект модели.
+    form = BirthdayForm(request.POST or None, instance=instance)
+    # Остальной код без изменений.
     context = {'form': form}
+    # Сохраняем данные, полученные из формы, и отправляем ответ:
     if form.is_valid():
-        form.save()          # save to DB
-    return render(request, 'birthday/birthday.html', context=context)
+        form.save()
+        birthday_countdown = calculate_birthday_countdown(
+            form.cleaned_data['birthday']
+        )
+        context.update({'birthday_countdown': birthday_countdown})
+    return render(request, 'birthday/birthday.html', context) 
 
 
 def birthday_list(request):
+    """Display records from DB."""
     # Получаем все объекты модели Birthday из БД.
     birthdays = Birthday.objects.all()
     # Передаём их в контекст шаблона.
     context = {'birthdays': birthdays}
     return render(request, 'birthday/birthday_list.html', context)
+
+
+def delete_birthday(request, pk):
+    """Delete record from DB."""
+    # Получаем объект модели или выбрасываем 404 ошибку.
+    instance = get_object_or_404(Birthday, pk=pk)
+    # В форму передаём только объект модели;
+    # передавать в форму параметры запроса не нужно.
+    form = BirthdayForm(instance=instance)
+    context = {'form': form}
+    # Если был получен POST-запрос...
+    if request.method == 'POST':
+        # ...удаляем объект:
+        instance.delete()
+        # ...и переадресовываем пользователя на страницу со списком записей.
+        return redirect('birthday:list')
+    # Если был получен GET-запрос — отображаем форму.
+    return render(request, 'birthday/birthday.html', context)
